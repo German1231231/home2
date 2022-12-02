a13eb134f1dc2725271b2b579957ee3033fa214b
German <german.glumov@gmail.com>
2022-12-03 01:42:05 +0300

    первый

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99 && number < 1000)
+{
+    int a = (number / 10) % 10;
+    Console.Write(a);
+}
+
+else
+{
+    Console.Write("Введенно не трёхзначное число");
+}
+
