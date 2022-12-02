5814f6d4317a0781e5d300d93629f557f15c04be
German <german.glumov@gmail.com>
2022-12-03 02:34:09 +0300

    Третье

---
+++ 3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 7 == 0 && number % 23 == 0)
+{
+    Console.Write("Да, число кратное одновременно 7 и 23");
+}
+
+else
+{
+    Console.Write("Нет, число не кратное одновременно 7 и 23");
+}
